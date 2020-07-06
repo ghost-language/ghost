8b83f85de64f2c6645f381b77c4d37998ccc09d9
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-07-05 19:35:52 -0700

    evaluate integer literals

---
+++ evaluator/evaluator.go
@@ -0,0 +1,34 @@
+package evaluator
+
+import (
+	"ghostlang.org/ghost/ast"
+
+	"ghostlang.org/ghost/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+
+	return result
+}
