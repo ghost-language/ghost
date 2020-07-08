933502007a96bc38d4b4467d382fc1bc59170cf2
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-07-08 10:39:28 -0700

    Add help flag and updated example fib scripts

--- ghost.go
+++ ghost.go
@@ -13,6 +13,7 @@ import (
 var (
 	flagVersion     bool
 	flagInteractive bool
+	flagHelp        bool
 )
 
 func init() {
@@ -22,6 +23,7 @@ func init() {
 		os.Exit(0)
 	}
 
+	flag.BoolVar(&flagHelp, "h", false, "display help information")
 	flag.BoolVar(&flagVersion, "v", false, "display version information")
 	flag.BoolVar(&flagInteractive, "i", false, "enable interactive mode")
 }
@@ -34,6 +36,11 @@ func main() {
 		os.Exit(0)
 	}
 
+	if flagHelp {
+		showHelp()
+		os.Exit(2)
+	}
+
 	args := flag.Args()
 	opts := &repl.Options{
 		Interactive: flagInteractive,
@@ -41,4 +48,34 @@ func main() {
 
 	repl := repl.New(args, opts)
 	repl.Run()
+}
+
+func showHelp() {
+	fmt.Println("Usage:")
+	fmt.Println()
+	fmt.Println("    ghost [flags] {file}")
+	fmt.Println()
+	fmt.Println("Flags:")
+	fmt.Println()
+	fmt.Println("    -h  show help")
+	fmt.Println("    -i  enter interactive mode after executing file")
+	fmt.Println("    -v  show version")
+	fmt.Println()
+	fmt.Println("Examples:")
+	fmt.Println()
+	fmt.Println("    ghost")
+	fmt.Println()
+	fmt.Println("            Start Ghost REPL")
+	fmt.Println()
+	fmt.Println("    ghost example.ghost")
+	fmt.Println()
+	fmt.Println("            Execute source file (example.ghost)")
+	fmt.Println()
+	fmt.Println("    ghost -i example.ghost")
+	fmt.Println()
+	fmt.Println("            Execute source file (example.ghost)")
+	fmt.Println("            and enter interactive mode (REPL)")
+	fmt.Println("            with the scripts environment intact")
+	fmt.Println()
+	fmt.Println()
 }
