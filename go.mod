829affab853209355ebfcd80852cae659ad5069c
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-02-17 09:15:48 -0800

    Once, There Was An Explosion, A Bang Which Gave Rise To Life As We Know It

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module ghostlang.org/x/ghost
 
 go 1.14
-
-require github.com/shopspring/decimal v1.2.0
--- token/token.go
+++ token/token.go
@@ -1,106 +1,66 @@
-// Package token defines constants representing the lexical tokens of the Ghost
-// programming language and basic operations on tokens.
 package token
 
-// TokenType represents the type of token.
-type TokenType string
+import "fmt"
 
-// Token represents the scanned token from source.
+// Type is the type of the token given as a string
+type Type string
+
+// All available tokens
+const (
+	// single-character tokens
+	LEFTPAREN  = "("
+	RIGHTPAREN = ")"
+	LEFTBRACE  = "{"
+	RIGHTBRACE = "}"
+	COMMA      = ","
+	DOT        = "."
+	MINUS      = "-"
+	PLUS       = "+"
+	SEMICOLON  = ";"
+	SLASH      = "/"
+	STAR       = "*"
+
+	// one or two character tokens
+	BANG         = "!"
+	BANGEQUAL    = "!="
+	EQUAL        = "="
+	EQUALEQUAL   = "=="
+	GREATER      = ">"
+	GREATEREQUAL = ">="
+	LESS         = "<"
+	LESSEQUAL    = "<="
+
+	// literals
+	IDENTIFIER = "IDENTIFIER"
+	STRING     = "STRING"
+	NUMBER     = "NUMBER"
+
+	// keywords
+	AND      = "and"
+	CLASS    = "class"
+	ELSE     = "else"
+	FALSE    = "false"
+	FUNCTION = "function"
+	FOR      = "for"
+	IF       = "if"
+	NULL     = "null"
+	OR       = "or"
+	RETURN   = "return"
+	SUPER    = "super"
+	THIS     = "this"
+	TRUE     = "true"
+	WHILE    = "while"
+	EOF      = "eof"
+)
+
+// Token contains the lexeme read by the scanner and records the line.
 type Token struct {
-	Type    TokenType
-	Literal string
+	Type    Type
+	Lexeme  string
+	Literal interface{}
 	Line    int
 }
 
-// The list of tokens.
-const (
-	// Special tokens
-	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF"
-
-	// Basic type literals
-	IDENTIFIER = "IDENTIFIER"
-	NUMBER     = "NUMBER"
-	STRING     = "STRING"
-
-	// Operators and delimiters
-	PLUS     = "+"
-	MINUS    = "-"
-	BANG     = "!"
-	ASTERISK = "*"
-	SLASH    = "/"
-	PERCENT  = "%"
-	DOT      = "."
-	HASH     = "#"
-
-	LT  = "<"
-	GT  = ">"
-	LTE = "<="
-	GTE = ">="
-
-	COMMA     = ","
-	SEMICOLON = ";"
-	COLON     = ":"
-
-	LPAREN   = "("
-	RPAREN   = ")"
-	LBRACE   = "{"
-	RBRACE   = "}"
-	LBRACKET = "["
-	RBRACKET = "]"
-
-	// Keywords
-	FUNCTION = "FUNCTION"
-	TRUE     = "TRUE"
-	FALSE    = "FALSE"
-	FOR      = "FOR"
-	IN       = "IN"
-	IF       = "IF"
-	ELSE     = "ELSE"
-	RETURN   = "RETURN"
-	WHILE    = "WHILE"
-	AND      = "AND"
-	OR       = "OR"
-	EXPORT   = "EXPORT"
-	IMPORT   = "IMPORT"
-
-	EQ    = "=="
-	NOTEQ = "!="
-
-	ASSIGN         = ":="
-	PLUSASSIGN     = "+="
-	MINUSASSIGN    = "-="
-	ASTERISKASSIGN = "*="
-	SLASHASSIGN    = "/="
-
-	PLUSPLUS   = "++"
-	MINUSMINUS = "--"
-
-	RANGE = ".."
-)
-
-var keywords = map[string]TokenType{
-	"function": FUNCTION,
-	"true":     TRUE,
-	"false":    FALSE,
-	"for":      FOR,
-	"in":       IN,
-	"if":       IF,
-	"else":     ELSE,
-	"return":   RETURN,
-	"while":    WHILE,
-	"and":      AND,
-	"or":       OR,
-	"export":   EXPORT,
-	"import":   IMPORT,
-}
-
-// LookupIdentifier checks the `keywords` table to see whether
-// the given identifier is in fact a keyword.
-func LookupIdentifier(identifier string) TokenType {
-	if token, ok := keywords[identifier]; ok {
-		return token
-	}
-
-	return IDENTIFIER
+func (token *Token) String() string {
+	return fmt.Sprintf("%s \"%s\" %v on line %d", token.Type, token.Lexeme, token.Literal, token.Line)
 }
