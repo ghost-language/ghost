bf36aa64fbb1514da452bd1cda81b5e50f642972
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2021-10-31 10:48:37 -0700

    Use correct value for string

--- parser/string.go
+++ parser/string.go
@@ -3,5 +3,5 @@ package parser
 import "ghostlang.org/x/ghost/ast"
 
 func (parser *Parser) stringLiteral() ast.ExpressionNode {
-	return &ast.String{Token: parser.current(), Value: parser.current().Lexeme}
+	return &ast.String{Token: parser.current(), Value: parser.current().Literal.(string)}
 }
