b18f61cc48b814bd372b2362fe5b3a9d0dcc0d7b
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2021-12-28 00:36:37 -0800

    add map index support

--- src/interpreter/index.go
+++ src/interpreter/index.go
@@ -25,15 +25,9 @@ func evaluateIndex(node *ast.Index, env *object.Environment) (object.Object, boo
 
 	switch {
 	case left.Type() == object.LIST && index.Type() == object.NUMBER:
-		list := left.(*object.List)
-		idx := index.(*object.Number).Value.IntPart()
-		max := int64(len(list.Elements) - 1)
-
-		if idx < 0 || idx > max {
-			return value.NULL, true
-		}
-
-		return list.Elements[idx], true
+		return evaluateListIndex(left, index)
+	case left.Type() == object.MAP:
+		return evaluateMapIndex(left, index)
 	default:
 		err := error.Error{
 			Reason:  error.Runtime,
@@ -44,4 +38,41 @@ func evaluateIndex(node *ast.Index, env *object.Environment) (object.Object, boo
 	}
 
 	return nil, false
+}
+
+func evaluateListIndex(left, index object.Object) (object.Object, bool) {
+	list := left.(*object.List)
+	idx := index.(*object.Number).Value.IntPart()
+	max := int64(len(list.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return value.NULL, true
+	}
+
+	return list.Elements[idx], true
+}
+
+func evaluateMapIndex(left, index object.Object) (object.Object, bool) {
+	mapObject := left.(*object.Map)
+
+	key, ok := index.(object.Mappable)
+
+	if !ok {
+		err := error.Error{
+			Reason:  error.Runtime,
+			Message: fmt.Sprintf("unusable as map key: %s", index.Type()),
+		}
+
+		log.Error(err.Reason, err.Message)
+
+		return nil, false
+	}
+
+	pair, ok := mapObject.Pairs[key.MapKey()]
+
+	if !ok {
+		return value.NULL, true
+	}
+
+	return pair.Value, true
 }
