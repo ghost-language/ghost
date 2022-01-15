f8414d76751fe1b2d956a1bd7e47bc1b0c3737c5
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2022-01-15 12:20:54 -0800

    evaluate super class

--- src/evaluator/class.go
+++ src/evaluator/class.go
@@ -13,6 +13,23 @@ func evaluateClass(node *ast.Class, scope *object.Scope) object.Object {
 		Super:       nil,
 	}
 
+	// super
+	if node.Super != nil {
+		identifier, ok := scope.Environment.Get(node.Super.Value)
+
+		if !ok {
+			object.NewError("%d:%d: runtime error: identifier '%s' not found in '%s'", node.Super.Token.Line, node.Super.Token.Column, scope.Self.String())
+		}
+
+		super, ok := identifier.(*object.Class)
+
+		if !ok {
+			object.NewError("%d:%d: runtime error: referenced identifier in extends not a class, got=%T", node.Super.Token.Line, node.Super.Token.Column, super)
+		}
+
+		class.Super = super
+	}
+
 	// Create a new scope for this class
 	classEnvironment := object.NewEnclosedEnvironment(scope.Environment)
 	classScope := &object.Scope{Environment: classEnvironment, Self: class}
