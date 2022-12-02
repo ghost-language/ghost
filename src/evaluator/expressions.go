23b348374fe6b1ee9719bab90cad1fbd062e08f9
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-12-02 22:04:50 +0800

    Add regexp support to evaluateExpressions
    This pull request adds support for regular expression values to the evaluateExpressions function. It checks if the evaluated expression is a string containing a regular expression pattern, and if it is, it compiles the regular expression and stores it in the result slice. If there is an error compiling the regular expression, it returns an error object. This allows the evaluateExpressions function to process regular expression values in a more straightforward and efficient manner.

--- src/evaluator/expressions.go
+++ src/evaluator/expressions.go
@@ -1,8 +1,7 @@
-package evaluator
-
 import (
 	"ghostlang.org/x/ghost/ast"
 	"ghostlang.org/x/ghost/object"
+	"regexp"
 )
 
 func evaluateExpressions(expressions []ast.ExpressionNode, scope *object.Scope) []object.Object {
@@ -10,6 +9,19 @@ func evaluateExpressions(expressions []ast.ExpressionNode, scope *object.Scope) 
 
 	for _, expression := range expressions {
 		evaluated := Evaluate(expression, scope)
+
+		// Check if the evaluated value is a string containing a regular expression pattern
+		if str, ok := evaluated.(*object.String); ok {
+			// If it is, compile the regular expression and store it in the result slice
+			regex, err := regexp.Compile(str.Value)
+			if err != nil {
+				// If there is an error compiling the regular expression,
+				// return an error object
+				return []object.Object{object.NewError(err.Error())}
+			}
+			result = append(result, &object.Regexp{Value: regex})
+			continue
+		}
 
 		if isError(evaluated) {
 			return []object.Object{evaluated}
